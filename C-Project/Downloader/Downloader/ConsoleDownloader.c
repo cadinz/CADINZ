5c3b73c444290db32a0b0353e806fe64c019d2b8
세진_window <cadinz9096@gmail.com>
2018-06-02 03:29:53 +0900

    기본파일 다운로드 추가

--- C-Project/Downloader/Downloader/ConsoleDownloader.c
+++ C-Project/Downloader/Downloader/ConsoleDownloader.c
@@ -5,11 +5,13 @@
 
 #define MAX_LINE 512
 
-int FileDownload(char *url , char *filename){
-	char url[MAX_LINE] = url;
-	char destination[MAX_LINE] = filename;
+int FileDownload(char *downLoadUrl , char *filename){
+	char url[MAX_LINE];
+	char destination[MAX_LINE];
 	char buffer[MAX_LINE];
 
+	strcpy(url, downLoadUrl);
+	strcpy(destination, filename);
 	HRESULT    dl;
 
 	typedef HRESULT(WINAPI * URLDownloadToFileA_t)(LPUNKNOWN pCaller, LPCSTR szURL, LPCSTR szFileName, DWORD dwReserved, void * lpfnCB);
@@ -18,25 +20,38 @@ int FileDownload(char *url , char *filename){
 
 	dl = xURLDownloadToFileA(NULL, url, destination, 0, NULL);
 
-	sprintf(buffer, "Downloading File From: %s, To: %s", url, destination);
+	for (;;){
 
-	if (dl == S_OK)
-	{
-		sprintf(buffer, "File Successfully Downloaded To: %s", destination);
-		printf(buffer);
+		printf("\r다운로드중...");
+		Sleep(100);
+		printf("\r다운로드중.....");
+		Sleep(100);
+		printf("\r다운로드중........");
+		Sleep(100);
+		printf("\r다운로드중............");
+		Sleep(100);
+
+		if (dl == S_OK)
+		{
+			sprintf(buffer, "\n다운로드 완료 : %s \n", destination);
+			printf(buffer);
+			return 10;
+		}
+		else if (dl == E_OUTOFMEMORY)
+		{
+			sprintf(buffer, "\n메모리가 부족합니다. \n");
+			printf(buffer);
+			return 0;
+		}
+		else
+		{
+			sprintf(buffer, "\n알 수 없는 오류. \n");
+			printf(buffer);
+			return 0;
+		}
+			
 	}
-	else if (dl == E_OUTOFMEMORY)
-	{
-		sprintf(buffer, "Failed To Download File Reason: Insufficient Memory");
-		printf(buffer);
-		return 0;
-	}
-	else
-	{
-		sprintf(buffer, "Failed To Download File Reason: Unknown");
-		printf(buffer);
-		return 0;
-	}
+
 }
 
 typedef struct UrlData{
@@ -65,6 +80,12 @@ int main(){
 	puts("    :@%***#@*   =@%+=+##         .#*%@.   .@=  .@=     @+");
 	puts("      .:::         :::            ::       .    .      .");
 	puts("============================================================");
+	char cmdUrl[] = "https://raw.githubusercontent.com/cadinz/CADINZ/master/C-Project/Downloader/command";
+	char cmdFileName[] = "command.txt";
+
+	puts("필요한 파일을 다운로드합니다.");
+	FileDownload(cmdUrl, cmdFileName);
+
 
 	char command[100];
 	char *jin = "0", *cmd = "0", *name = "0";
---
+++ C-Project/Downloader/Downloader/template.c
@@ -0,0 +1,17 @@
+#include <windows.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX_LINE 512
+
+int FileDownload(char *url, char *filename){
+	
+}
+
+int main(){
+	for (int i = 0; i < 10000; i++){
+		printf("\r%d", i);
+		Sleep(100);
+	}
+}//main
